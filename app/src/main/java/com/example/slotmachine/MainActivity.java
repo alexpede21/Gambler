c95fd851eca84ad4790dce74443b4538e3fe663a
stpedale <stpedale@bx.fallmerayer.it>
2024-05-28 12:27:55 +0200

    Slot Game

--- app/src/main/java/com/example/slotmachine/MainActivity.java
+++ app/src/main/java/com/example/slotmachine/MainActivity.java
@@ -5,13 +5,19 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private double currentBet = 0;
+    private TextView currentBetTextView;
 
-    private ImageView slot1, slot2, slot3;
+    private ImageView[] slots;
     private Button spinButton;
+    private Button plusButton;
+    private Button minusButton;
     private int[] images = {R.drawable.bar, R.drawable.cherry, R.drawable.diamond, R.drawable.lemon,
             R.drawable.orange, R.drawable.seven};
     private Random random = new Random();
@@ -22,10 +28,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        slot1 = findViewById(R.id.slot1);
-        slot2 = findViewById(R.id.slot2);
-        slot3 = findViewById(R.id.slot3);
+        slots = new ImageView[] {
+                findViewById(R.id.slot1_top), findViewById(R.id.slot2_top), findViewById(R.id.slot3_top),
+                findViewById(R.id.slot1_middle), findViewById(R.id.slot2_middle), findViewById(R.id.slot3_middle),
+                findViewById(R.id.slot1_bottom), findViewById(R.id.slot2_bottom), findViewById(R.id.slot3_bottom)
+        };
+
         spinButton = findViewById(R.id.spinButton);
+        plusButton = findViewById(R.id.plusButton);
+        minusButton = findViewById(R.id.minusButton);
+
+        currentBetTextView = findViewById(R.id.current_bet);
 
         spinButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +46,34 @@ public class MainActivity extends AppCompatActivity {
                 spinSlots();
             }
         });
+
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addBet();
+            }
+        });
+
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subtractBet();
+            }
+        });
+    }
+
+    private void addBet(){
+        currentBet += 0.25;
+        updateBetText();
+    }
+
+    private void subtractBet(){
+        currentBet -= 0.25;
+        updateBetText();
+    }
+
+    private void updateBetText(){
+        currentBetTextView.setText("Current Bet: " + currentBet + "$");
     }
 
     private void spinSlots() {
@@ -40,14 +81,10 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                int imageIndex1 = random.nextInt(images.length);
-                int imageIndex2 = random.nextInt(images.length);
-                int imageIndex3 = random.nextInt(images.length);
-
-                slot1.setImageResource(images[imageIndex1]);
-                slot2.setImageResource(images[imageIndex2]);
-                slot3.setImageResource(images[imageIndex3]);
-
+                for (ImageView slot : slots) {
+                    int imageIndex = random.nextInt(images.length);
+                    slot.setImageResource(images[imageIndex]);
+                }
                 spinButton.setEnabled(true);
             }
         }, 1000); // 1 second delay to simulate spinning
